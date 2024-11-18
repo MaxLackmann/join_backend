075243991624669c84fe4c4ab05bdbf40df1d11c
MaxLackmann <lackmann_max@hotmail.com>
2024-11-18 14:09:16 +0100

    changed

--- join_app/api/views.py
+++ join_app/api/views.py
@@ -5,25 +5,6 @@ from join_app.api.serializers import ContactSerializer, TaskSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = ContactSerializer
-    
-    def get_queryset(self):
-        """Fügt sicherheitshalber den Gastbenutzer hinzu, falls er nicht existiert."""
-        User.objects.get_or_create(
-            username="guest",
-            defaults={"email": "guest@example.com", "password": ""}
-        )
-        return super().get_queryset()
-
-    def retrieve(self, request, *args, **kwargs):
-        """Behandle Gastbenutzer-Abfrage separat."""
-        if kwargs.get("pk") == "guest":
-            guest_user, created = User.objects.get_or_create(
-                username="guest",
-                defaults={"email": "guest@example.com", "password": ""}
-            )
-            serializer = self.get_serializer(guest_user)
-            return Response(serializer.data)
-        return super().retrieve(request, *args, **kwargs)
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
--- join_app/models.py
+++ join_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User (models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=100)
     
     def __str__(self):
         return self.username
